95983307e06d2a21e9da1d52ad030384a17fca56
Oscar <oscarbranson@gmail.com>
2018-03-16 12:05:43 +1100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='latools_gui',
+      version='0.0.1',
+      description='A GUI for LAtools',
+      url='https://github.com/oscarbranson/latools_gui',
+      author='TechLauncher Project - Team XXXXX',
+      author_email='oscarbranson@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Science/Research',
+                   'Topic :: Scientific/Engineering',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 3',
+                   ],
+      install_requires=['latools',
+                       ],
+      # package_data={
+      # },
+      zip_safe=False)
