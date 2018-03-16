0b3ebcd4f0afb60b07ac2f080d647dff0e9ba7b4
Oscar <oscarbranson@gmail.com>
2018-03-16 12:08:46 +1100

    rtfd theme, use napolean for autodox

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,6 +45,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon'  # support for google and numpy docstring styles
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -80,7 +81,8 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
