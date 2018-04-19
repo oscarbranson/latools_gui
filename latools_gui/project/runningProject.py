327a763d5227d8178d1c4f8f52639a2bc19ed181
Ilya Sergeev <u6050215@anu.edu.au>
2018-04-19 17:30:20 +1000

    Saving stuff

--- latools_gui/project/runningProject.py
+++ latools_gui/project/runningProject.py
@@ -1,33 +1,35 @@
 import os
 import time
 import json
+import latools
+import re
 
 class RunningProject():
-        """
-        This class is intended to house everything that is specific to one project.
-        When a project is created or loaded, this class is what is created or loaded.
-        The instance of this class is passed to each stage.
-        Currently it just contains a class variable which is later assigned as the la.analyse object.
-        """
+	"""
+	This class is intended to house everything that is specific to one project.
+	When a project is created or loaded, this class is what is created or loaded.
+	The instance of this class is passed to each stage.
+	Currently it just contains a class variable which is later assigned as the la.analyse object.
+	"""
 
-        
-        def __init__(self):
-                """ Initialise a blank unaltered state.
+	
+	def __init__(self):
+		""" Initialise a blank unaltered state.
 
 		Creates tabula rasa project state.
 
 		Attributes
 		----------
 		dataDictionary : dict
-                        A dictionary meant to store var pairs.
-                        Defined by calls from outside stages' needs.
+			A dictionary meant to store var pairs.
+			Defined by calls from outside stages' needs.
 		"""
-                
-                self.eg = None
-                self.dataDictionary = {}
+		
+		self.eg = None
+		self.dataDictionary = {}
 
-        def updateSetting (self, key, value):
-                """ The call to update variables stored within
+	def updateSetting (self, key, value):
+		""" The call to update variables stored within
 
 		To change or add new data to be saved,
 		this method should be called upon in prime.
@@ -40,10 +42,15 @@ class RunningProject():
 			What data to be locked under such key                
 		"""
 
-                self.dataDictionary[key] = value
+		self.dataDictionary[key] = value
 
-        def readSetting(self, key):
-                """ Recall the variables stored within the dict
+	#def massUpdate(self, path):
+		
+		
+					   
+
+	def readSetting(self, key):
+		""" Recall the variables stored within the dict
 
 		To read and query data saved before
 		this method's call returns a value stored.
@@ -59,18 +66,20 @@ class RunningProject():
 			gives None when probes an empty memory.
 		"""
 
-                if key in self.dataDictionary:
-                        
-                        return self.dataDictionary[key]
-                else:
-                        return None
+		if key in self.dataDictionary:
 
-        def saveProject(self, path = None):
-                """ The call which saves stored data to a file
+			print ('key found, value: ' + self.dataDictionary[key])
+			return self.dataDictionary[key]
+		else:
+			print ('key not found')
+			return None
 
-		Saves the project state,
-		path defaults to own folder.
-		Chose to use json.
+	def saveProject(self, path = None, name = 'default.sav'):
+		""" The call which saves stored data to a file
+
+		Saves the project dict
+		Deprecated, using logs
+		Ignore this function.
 
 		Parameters
 		----------
@@ -80,40 +89,61 @@ class RunningProject():
 			Non-default value best supplied in call.
 		"""
 
-                if path is None:
-                        path = 'projects/default.sav'
-                if not os.path.isdir(path):
-                       os.mkdir(path)
+		if path is None:
+			path = 'projects/'
+		if not os.path.isdir(path):
+		       os.mkdir(path)
 
-                header = ['# Sample save file for testing purposes created %s\n' % (time.strftime('%Y:%m:%d %H:%M:%S)'))]
-                
+		header = ['# Sample save file for testing purposes created %s\n' % (time.strftime('%Y:%m:%d %H:%M:%S)'))]
+		
 
-                with open(path + name + '.sav', 'w') as f:
-                        f.write('\n'.join(header))
-                        json.dump(self.dataDictionary, f)
-                        f.close()
+		with open(path + name, 'w') as f:
+			f.write('\n'.join(header))
+			json.dump(self.dataDictionary, f)
+			f.close()
 
-        def loadProject(self, path = None):
-                """ The call which loads stored data from a file
+	def loadProject(self, path = None, name = 'analysis.log'):
+		""" The call which loads stored data from a file
 
-		To recall data from an outside file,
-		call method hence and supply path to file.
-		A choice was made to favour json's use.
+		Loads the log data
+		Translates inbuilt logs to dict
+		No more fool json.
 
 		Parameters
 		----------
 		path : str
-			The path which points to where the file is laid.
-			Its declaration's place might yet be changed.
-                        Has default value only fit for tests.
+			Path to where file is
+			Leads to its directory.
+			Default not advised.
+		name : str
+                        Name of loaded file
+                        Points to established format
+                        Should leave as default?
 		"""
 
-                if path is None:
-                        path = 'projects/default.sav'
+		if path is None:
+			path = 'data_export/minimal_export/'
+			#path = 'exp/'
+		if os.path.isdir(path):
+			with open(path + name, 'r') as f:
+				rlog = f.readlines()
+			hashind = [i for i, n in enumerate(rlog) if '#' in n]
+			self.dataDictionary = {}
+			#print (i)
+			logread = re.compile('([a-z_]+) :: args=(\(.*\)) kwargs=(\{.*\})')
+			
+			#a = eval(logread.match(log[hashind[1] + 1]).groups()[-1])
+			for l in rlog[hashind[1] + 2:]:
+				fname, args, kwargs = logread.match(l).groups()
+				self.dataDictionary.update(**eval(kwargs))
+					
+			#print (a)
+			#self.dataDictionary = a
+			#print (self.dataDictionary)
+			print ('loading done')
+		else:
+			print ("git rekt")  #not a formal error message
 
-                with open(path + name + '.sav', 'r') as f:
-                        print (f.readline())
-                        self.dataDictionary = json.load(f)
-                        f.close()
-                        
-
+#a = RunningProject()
+#a.loadProject()
+#print (a.readSetting('exponent'))
--- latools_gui/stages/despikingStage.py
+++ latools_gui/stages/despikingStage.py
@@ -102,24 +102,32 @@ class DespikingStage():
 		self.applyButton.clicked.connect(self.pressedApplyButton)
 		self.stageControls.addApplyButton(self.applyButton)
 
+		self.loadTest = QPushButton("TEST LOAD")
+		self.loadTest.clicked.connect(self.loadApplication)
+		self.stageControls.addApplyButton(self.loadTest)
+
 	def pressedApplyButton(self):
 
 
 		localExponent = None
 		if (self.pane1Exponent.text() != ""):
 			localExponent = float(self.pane1Exponent.text())
+		self.project.updateSetting("exponent", localExponent)
 
 		localWin = 3
 		if (self.pane2win.text() != ""):
 			localWin = float(self.pane2win.text())
+		self.project.updateSetting("win", localWin)
 
 		localNlim = 12
 		if (self.pane2nlim.text() != ""):
 			localNlim = int(self.pane2nlim.text())
+		self.project.updateSetting("nlim", localNlim)
 
 		localMaxiter = 4
 		if (self.pane1Maxiter.text() != ""):
 			localMaxiter = int(self.pane1Maxiter.text())
+		self.project.updateSetting("maxiter", localMaxiter)
 
 
 		self.project.eg.despike(expdecay_despiker=self.pane1expdecayOption.isChecked(),
@@ -132,4 +140,48 @@ class DespikingStage():
 
 		self.graphPaneObj.updateGraph('despiked')
 		self.navigationPaneObj.setRightEnabled()
+
+	def loadApplication(self):
+
+                """ Load preconfigured data into fields
+
+                Load project field states
+                Updates GUI from project
+                Cloned for all stages?
+
+                Currently unused, left in as an example. Can be deleted if not wanted.
+
+                Attributes
+                ----------
+                localExponent : float
+                        LATools' despiking 'exponent'
+                localWin: float
+                        LATools' despiking 'win'
+                localNlim : float
+                        LATools' despiking 'nlim'
+
+                """
+		
+		localExponent = 0.0
+		#print ('local exponent defaulted')
+		if self.project.dataDictionary['exponent'] is not None:
+			#print ('exponent value found!')
+			localExponent = self.project.dataDictionary['exponent']
+			#print (localExponent)
+		self.pane1Exponent.setText(str(localExponent))
+		#print ('exponent set as ' + str(localExponent))
+		
+		localWin = 3.0
+		if self.project.dataDictionary['win'] is not None:
+			localWin = self.project.dataDictionary['win']
+			#print ('localWin read as ' + str(localWin))
+		self.pane2win.setText(str(localWin))
+		#print ('localWin set as ' + str(localWin))
+
+		localNlim = 12
+		if self.project.dataDictionary['nlim'] is not None:
+			localNlim = self.project.dataDictionary['nlim']
+			#print ('nlim read as ' + str(localNlim))
+		self.pane2nlim.setText(str(localNlim))
+		#print ('nlim set as ' + str(localNlim))
 
