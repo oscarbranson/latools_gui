847a3830cb293d4f0adcbb32f4a74ecceb918883
Simon Petocz <u4537268@anu.edu.au>
2018-08-06 21:01:41 +1000

    All four threshold filters and their options are in place

--- latools_gui/filters/thresholdFilter.py
+++ latools_gui/filters/thresholdFilter.py
@@ -32,6 +32,9 @@ class ThresholdFilter:
 		# Determines if the filter has been created yet
 		self.created = False
 
+		# Prevents updating based on the program activating check boxes
+		self.updating = False
+
 		# We create each checkbox that will appear in the filter controls and summary tabs
 		for i in range(len(self.filterTab.project.eg.analytes)):
 
@@ -53,11 +56,12 @@ class ThresholdFilter:
 
 		# A combobox for the type
 		self.typeCombo = QComboBox()
+		self.typeCombo.activated.connect(self.typeChanged)
 
 		self.thresholdTypes = ["Threshold",
-							   "Threshold percentile",
-							   "Threshold Gradient",
-							   "Threshold Gradient Percentile"]
+							   "Threshold Percentile",
+							   "Gradient Threshold",
+							   "Gradient Threshold Percentile"]
 
 		for t in self.thresholdTypes:
 			self.typeCombo.addItem(t)
@@ -74,16 +78,17 @@ class ThresholdFilter:
 		self.threshValueEdit = QLineEdit()
 		self.optionsLayout.addWidget(self.threshValueEdit, 1, 1)
 		self.threshValueEdit.setMaximumWidth(100)
+		self.threshValueEdit.setPlaceholderText("float")
 
 		self.threshValueLabel.setToolTip(self.filterTab.filterInfo["threshold_value_description"])
 		self.threshValueEdit.setToolTip(self.filterTab.filterInfo["threshold_value_description"])
 
 		# An option for what analyte to base the filter on
 		self.analyteLabel = QLabel(self.filterTab.filterInfo["analyte_label"])
-		self.optionsLayout.addWidget(self.analyteLabel, 2, 0)
+		self.optionsLayout.addWidget(self.analyteLabel, 0, 2)
 
 		self.analyteCombo = QComboBox()
-		self.optionsLayout.addWidget(self.analyteCombo, 2, 1)
+		self.optionsLayout.addWidget(self.analyteCombo, 0, 3)
 
 		self.analyteCombo.addItem(" ")
 		for i in range(len(self.filterTab.project.eg.analytes)):
@@ -92,7 +97,40 @@ class ThresholdFilter:
 		self.analyteLabel.setToolTip(self.filterTab.filterInfo["analyte_description"])
 		self.analyteCombo.setToolTip(self.filterTab.filterInfo["analyte_description"])
 
-		self.optionsLayout.setColumnStretch(2, 1)
+		# An option for the percentile
+		self.percentLabel = QLabel(self.filterTab.filterInfo["percentile_label"])
+		self.percentEdit = QLineEdit()
+		self.optionsLayout.addWidget(self.percentLabel, 2, 0)
+		self.optionsLayout.addWidget(self.percentEdit, 2, 1)
+		self.percentEdit.setPlaceholderText("float")
+		self.percentEdit.setEnabled(False)
+
+		self.percentLabel.setToolTip(self.filterTab.filterInfo["percentile_description"])
+		self.percentEdit.setToolTip(self.filterTab.filterInfo["percentile_description"])
+
+		# An option for the window
+		self.winLabel = QLabel(self.filterTab.filterInfo["win_label"])
+		self.winEdit = QLineEdit("15")
+		self.optionsLayout.addWidget(self.winLabel, 1, 2)
+		self.optionsLayout.addWidget(self.winEdit, 1, 3)
+		self.winEdit.setEnabled(False)
+
+		self.winLabel.setToolTip(self.filterTab.filterInfo["percentile_description"])
+		self.winEdit.setToolTip(self.filterTab.filterInfo["percentile_description"])
+
+		# An option for the level
+		self.levelLabel = QLabel(self.filterTab.filterInfo["level_label"])
+		self.levelCombo = QComboBox()
+		self.levelCombo.addItem("population")
+		self.levelCombo.addItem("individual")
+		self.optionsLayout.addWidget(self.levelLabel, 2, 2)
+		self.optionsLayout.addWidget(self.levelCombo, 2, 3)
+		self.levelLabel.setToolTip(self.filterTab.filterInfo["level_description"])
+		self.levelCombo.setToolTip(self.filterTab.filterInfo["level_description"])
+		self.levelCombo.setEnabled(False)
+
+		# We add a stretch that will fill any extra space on the right-most column
+		self.optionsLayout.setColumnStretch(4, 1)
 
 		# We create the control buttons
 		self.crossPlotButton = QPushButton("Cross-plot")
@@ -125,54 +163,119 @@ class ThresholdFilter:
 
 	def controlChecksRegister(self):
 		""" Sets the checkboxes in the Summary tab to be the same as those in the Controls tab """
+		if not self.updating:
+			self.updating = True
+			for i in range(len(self.analyteCheckBoxes["controlsAbove"])):
 
-		for i in range(len(self.analyteCheckBoxes["controlsAbove"])):
+				self.analyteCheckBoxes["summaryAbove"][i].setCheckState(
+					self.analyteCheckBoxes["controlsAbove"][i].checkState())
 
-			self.analyteCheckBoxes["summaryAbove"][i].setCheckState(
-				self.analyteCheckBoxes["controlsAbove"][i].checkState())
+				self.analyteCheckBoxes["summaryBelow"][i].setCheckState(
+					self.analyteCheckBoxes["controlsBelow"][i].checkState())
 
-			self.analyteCheckBoxes["summaryBelow"][i].setCheckState(
-				self.analyteCheckBoxes["controlsBelow"][i].checkState())
+			self.updateAnalyteToggles()
+			self.updating = False
 
-		self.updateAnalyteToggles()
 
 	def summaryChecksRegister(self):
 		""" Sets the checkboxes in the Controls tab to be the same as those in the Summary tab """
+		if not self.updating:
+			self.updating = True
+			for i in range(len(self.analyteCheckBoxes["controlsAbove"])):
+				self.analyteCheckBoxes["controlsAbove"][i].setCheckState(
+					self.analyteCheckBoxes["summaryAbove"][i].checkState())
 
-		for i in range(len(self.analyteCheckBoxes["controlsAbove"])):
-			self.analyteCheckBoxes["controlsAbove"][i].setCheckState(
-				self.analyteCheckBoxes["summaryAbove"][i].checkState())
+				self.analyteCheckBoxes["controlsBelow"][i].setCheckState(
+					self.analyteCheckBoxes["summaryBelow"][i].checkState())
 
-			self.analyteCheckBoxes["controlsBelow"][i].setCheckState(
-				self.analyteCheckBoxes["summaryBelow"][i].checkState())
-
-		self.updateAnalyteToggles()
+			self.updateAnalyteToggles()
+			self.updating = False
 
 	def createClick(self):
 		""" Adds the new filter to the Summary tab """
 
-
-		# Check the input options
-		localThreshold = 0.0
-		try:
-			localThreshold = float(self.threshValueEdit.text())
-		except:
-			self.raiseError("The 'Threshold' value must be a floating point number")
-			return
-
+		# All threshold filter types need an analyte selected
 		if self.analyteCombo.currentText() == " ":
 			self.raiseError("You must select an analyte")
 			return
 
-		# We create the filter
-		# TO DO: ADD OTHER THRESHOLD TYPES
-		self.filterTab.project.eg.filter_threshold(analyte = self.analyteCombo.currentText(),
-												   threshold = localThreshold)
+		# We now run a different function based on the selected threshold type
 
-		self.filtNameAbove = list(self.filterTab.project.eg.data['Sample-1'].filt.components.keys())[-1]
-		self.filtNameBelow = list(self.filterTab.project.eg.data['Sample-1'].filt.components.keys())[-2]
+		# Threshold filter selected:
+		if self.typeCombo.currentText() == self.thresholdTypes[0]:
 
-		# print("Above: " + self.filtNameAbove + " Below: " + self.filtNameBelow)
+			try:
+				localThreshold = float(self.threshValueEdit.text())
+			except:
+				self.raiseError("The 'Threshold' value must be a floating point number")
+				return
+
+			# We create the filter
+			self.filterTab.project.eg.filter_threshold(analyte = self.analyteCombo.currentText(),
+													   threshold = localThreshold)
+
+		# Threshold Percentile filter selected:
+		elif self.typeCombo.currentText() == self.thresholdTypes[1]:
+
+			try:
+				localPercent = [float(self.percentEdit.text())]
+			except:
+				self.raiseError("The 'Percentile' value must be a floating point number")
+				return
+
+			# We create the filter
+			self.filterTab.project.eg.filter_threshold_percentile(analyte=self.analyteCombo.currentText(),
+																  percentiles=localPercent,
+																  level=self.levelCombo.currentText())
+
+		# Gradient Threshold filter selected:
+		elif self.typeCombo.currentText() == self.thresholdTypes[2]:
+
+			try:
+				localThreshold = float(self.threshValueEdit.text())
+			except:
+				self.raiseError("The 'Threshold' value must be a floating point number")
+				return
+
+			try:
+				localWin = int(self.winEdit.text())
+			except:
+				self.raiseError("The 'Window' value must be an integer")
+				return
+
+			# We create the filter
+			self.filterTab.project.eg.filter_gradient_threshold(analyte=self.analyteCombo.currentText(),
+																threshold=localThreshold,
+																win=localWin)
+
+		# Gradient Threshold Percentile filter selected:
+		elif self.typeCombo.currentText() == self.thresholdTypes[2]:
+
+			try:
+				localPercent = [float(self.percentEdit.text())]
+			except:
+				self.raiseError("The 'Percentile' value must be a floating point number")
+				return
+
+			try:
+				localWin = int(self.winEdit.text())
+			except:
+				self.raiseError("The 'Window' value must be an integer")
+				return
+
+			# We create the filter
+			self.filterTab.project.eg.filter_gradient_threshold_percentile(analyte=self.analyteCombo.currentText(),
+																		   percentiles=localPercent,
+																		   level=self.levelCombo.currentText(),
+																		   win=localWin)
+
+		# To determine the name that LAtools has given the filter, we first take a sample:
+		egSubset = self.filterTab.project.eg.subsets['All_Samples'][0]
+		print(self.filterTab.project.eg.data[egSubset].filt.components.keys())
+		
+		# Then check the last filter names that have been added to that sample:
+		self.filtNameAbove = list(self.filterTab.project.eg.data[egSubset].filt.components.keys())[-1]
+		self.filtNameBelow = list(self.filterTab.project.eg.data[egSubset].filt.components.keys())[-2]
 
 		# We toggle the analytes on and off based on the check boxes
 		self.updateAnalyteToggles()
@@ -203,30 +306,61 @@ class ThresholdFilter:
 
 	def raiseError(self, message):
 		""" Creates an error box with the given message """
-		errorBox = QMessageBox.critical(self.filterTab, "Error", message, QMessageBox.Ok)
+		errorBox = QMessageBox.critical(self.filterTab.filter, "Error", message, QMessageBox.Ok)
 
 	def updateAnalyteToggles(self):
 		""" Updates each analyte in the filter to conform to the state of that analyte's checkbox """
 
-		#print("updating filter analytes")
+		if self.created:
 
-		for i in range(len(self.filterTab.project.eg.analytes)):
-			# We update for the "Above" filter
-			# If the checkbox is not unchecked...
-			if self.analyteCheckBoxes["summaryAbove"][i].checkState() != 0:
-				self.filterTab.project.eg.filter_on(self.filtNameAbove,
-														 self.filterTab.project.eg.analytes[i])
-			else:
-				self.filterTab.project.eg.filter_off(self.filtNameAbove,
-														 self.filterTab.project.eg.analytes[i])
+			for i in range(len(self.filterTab.project.eg.analytes)):
+				# We update for the "Above" filter
+				# If the checkbox is not unchecked...
+				if self.analyteCheckBoxes["summaryAbove"][i].checkState() != 0:
+					self.filterTab.project.eg.filter_on(self.filtNameAbove,
+															 self.filterTab.project.eg.analytes[i])
+				else:
+					self.filterTab.project.eg.filter_off(self.filtNameAbove,
+															 self.filterTab.project.eg.analytes[i])
 
-			# Update for the "Below" filter
-			if self.analyteCheckBoxes["summaryBelow"][i].checkState() != 0:
-				self.filterTab.project.eg.filter_on(self.filtNameBelow,
-														 self.filterTab.project.eg.analytes[i])
-			else:
-				self.filterTab.project.eg.filter_off(self.filtNameBelow,
-														 self.filterTab.project.eg.analytes[i])
+				# Update for the "Below" filter
+				if self.analyteCheckBoxes["summaryBelow"][i].checkState() != 0:
+					self.filterTab.project.eg.filter_on(self.filtNameBelow,
+															 self.filterTab.project.eg.analytes[i])
+				else:
+					self.filterTab.project.eg.filter_off(self.filtNameBelow,
+															 self.filterTab.project.eg.analytes[i])
+
+	def typeChanged(self):
+		""" Sets the enabled options based on what threshold type is currently selected """
+
+		if self.typeCombo.currentText() == self.thresholdTypes[0]:
+			# filter = Threshold
+			self.threshValueEdit.setEnabled(True)
+			self.percentEdit.setEnabled(False)
+			self.winEdit.setEnabled(False)
+			self.levelCombo.setEnabled(False)
+
+		elif self.typeCombo.currentText() == self.thresholdTypes[1]:
+			# filter = Threshold Percentile
+			self.threshValueEdit.setEnabled(False)
+			self.percentEdit.setEnabled(True)
+			self.winEdit.setEnabled(False)
+			self.levelCombo.setEnabled(True)
+
+		elif self.typeCombo.currentText() == self.thresholdTypes[2]:
+			# filter = Gradient Threshold
+			self.threshValueEdit.setEnabled(True)
+			self.percentEdit.setEnabled(False)
+			self.winEdit.setEnabled(True)
+			self.levelCombo.setEnabled(False)
+
+		elif self.typeCombo.currentText() == self.thresholdTypes[3]:
+			# filter = Threshold Gradient Percentile
+			self.threshValueEdit.setEnabled(False)
+			self.percentEdit.setEnabled(True)
+			self.winEdit.setEnabled(True)
+			self.levelCombo.setEnabled(True)
 
 	def crossPlotClick(self):
 		""" Activates when the Cross-plot button is pressed """
